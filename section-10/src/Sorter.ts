81a736f0e1beadbd09b49466c01df18bceb2b99d
Charae <charae@unienrol.com>
2024-02-05 13:45:56 +0800

    feat: section 10@101, add comment for interface v abstract class

--- section-10/src/Sorter.ts
+++ section-10/src/Sorter.ts
@@ -1,9 +1,16 @@
+// ! No longer being used, can be removed
 export interface Sortable {
   length: number;
   compare(leftIndex: number, rightIndex: number): boolean;
   swap(leftIndex: number, rightIndex: number): void;
 }
 
+/**
+ * Why abstract class instead of just using interface?
+ * Because the classes are kinda related. We use abstract class when we are trying to build up
+ * a definition of an object, since it couples classes together.
+ * Meanwhile, we use interface if we have different objects that we want to work together (like the user and company example before)
+ */
 export abstract class Sorter {
   abstract length: number;
   abstract compare(leftIndex: number, rightIndex: number): boolean;
