e5548da286522da7cf13540439f44efc4dc15bbf
Charae <charae@unienrol.com>
2024-02-06 10:34:04 +0800

    feat: section15#239, basic decorator to log error

---
+++ section-15/decorators.ts
@@ -0,0 +1,27 @@
+class Boat {
+  color = 'red';
+
+  get formattedColor(): string {
+    return `This boat color is ${this.color}`;
+  }
+
+  @logError
+  pilot(): void {
+    throw new Error();
+    console.log('pilot!');
+  }
+}
+
+function logError(target: any, key: string, desc: PropertyDescriptor): void {
+  const method = desc.value;
+
+  desc.value = function () {
+    try {
+      method();
+    } catch (err) {
+      console.error('Error');
+    }
+  };
+}
+
+new Boat().pilot();
