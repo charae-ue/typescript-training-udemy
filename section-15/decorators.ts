0cd6a29709292972e8f9bc31a0193978ed4f7d94
Charae <charae@unienrol.com>
2024-02-06 10:38:28 +0800

    feat: section 15#240, add decorator factory
    Allow passing in args to decorator, e.g. for custom error message

--- section-15/decorators.ts
+++ section-15/decorators.ts
@@ -5,22 +5,26 @@ class Boat {
     return `This boat color is ${this.color}`;
   }
 
-  @logError
+  @logError('Pilot error hah!')
   pilot(): void {
     throw new Error();
     console.log('pilot!');
   }
 }
 
-function logError(target: any, key: string, desc: PropertyDescriptor): void {
-  const method = desc.value;
+// Decorator factory, allow us to pass args to decorator (e.g. for passing log error message)
+function logError(errorMessage: string) {
+  return function (target: any, key: string, desc: PropertyDescriptor): void {
+    // console.log(desc)
+    const method = desc.value;
 
-  desc.value = function () {
-    try {
-      method();
-    } catch (err) {
-      console.error('Error');
-    }
+    desc.value = function () {
+      try {
+        method();
+      } catch (err) {
+        console.error(errorMessage);
+      }
+    };
   };
 }
 
